e1d440f8cf459542ce0eaf16c5962b854cd549cf
calvinpierce111 <calvinpierce111@gmail.com>
2026-05-06 11:51:38 +0300

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+// Очистить старый кеш и сразу активироваться
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
+
+// Не кешировать ничего — всегда брать из сети
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
